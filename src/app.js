b8c4bd0a6157bf21138f5c5f12ec7d4384abb42b
Caleb Holm <calebcholm@gmail.com>
2022-08-14 17:14:10 -0500

    Added CORS package in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 if (process.env.USER) require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 //routers
@@ -11,6 +12,7 @@ const theatersRouter = require('./theaters/theaters.router');
 const errorHandler = require('./errors/errorHandler');
 const notFound = require('./errors/notFound');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/movies', moviesRouter);
