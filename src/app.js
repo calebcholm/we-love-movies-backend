ecf391770003c60ad376325b041b6e29f4f6bf7b
Caleb Holm <calebcholm@gmail.com>
2022-08-19 21:45:26 -0500

    Added public / static frontend

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const notFound = require('./errors/notFound');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/movies', moviesRouter);
 app.use('/reviews', reviewsRouter);
