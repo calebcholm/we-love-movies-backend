676a543f9f6845613bf4551eae87c138ac66b212
Caleb Holm <calebcholm@gmail.com>
2022-08-20 16:42:15 -0500

    testing db url

--- knexfile.js
+++ knexfile.js
@@ -2,45 +2,47 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  // DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = 'process.env.DATABASE_URL'
 } = process.env;
+ 
 
-module.exports = {
-  development: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+  (module.exports = {
+    development: {
+      client: 'postgresql',
+      connection: DATABASE_URL,
+      pool: { min: 0, max: 5 },
+      migrations: {
+        directory: path.join(__dirname, 'src', 'db', 'migrations'),
+      },
+      seeds: {
+        directory: path.join(__dirname, 'src', 'db', 'seeds'),
+      },
     },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-  },
 
-  production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+    production: {
+      client: 'postgresql',
+      connection: DATABASE_URL,
+      pool: { min: 0, max: 5 },
+      migrations: {
+        directory: path.join(__dirname, 'src', 'db', 'migrations'),
+      },
+      seeds: {
+        directory: path.join(__dirname, 'src', 'db', 'seeds'),
+      },
     },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-  },
 
-  test: {
-    client: "sqlite3",
-    connection: {
-      filename: ":memory:",
+    test: {
+      client: 'sqlite3',
+      connection: {
+        filename: ':memory:',
+      },
+      migrations: {
+        directory: path.join(__dirname, 'src', 'db', 'migrations'),
+      },
+      seeds: {
+        directory: path.join(__dirname, 'src', 'db', 'seeds'),
+      },
+      useNullAsDefault: true,
     },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
-  },
-};
+  });
