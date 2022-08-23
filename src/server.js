a4e682edf3c29dc9506d174fddc8ee7109d4e92f
Caleb Holm <calebcholm@gmail.com>
2022-08-22 21:53:50 -0500

    Undo

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
-const PORT = process.env.PORT || 5000;
+const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
-const listener = () => console.log(`Listening on Port ${PORT}!`);
 
-app.set('port', PORT);
+const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(process.env.PORT || PORT, listener);
+    app.listen(PORT, listener);
   })
   .catch(console.error);
